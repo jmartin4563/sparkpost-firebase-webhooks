44b29333a90f9f91bada2df09b1b8b1f80e2fda9
Jessica Martin <jessica.martin@messagesystems.com>
2016-01-30 01:09:23 -0500

    make it not look like crap, stub out handler functions for each event type

---
+++ README.md
@@ -0,0 +1,1 @@
+# So I heard you like mudki... I mean... webhooks.
--- index.js
+++ index.js
@@ -1,56 +1,136 @@
 var Firebase = require('firebase')
-  , db = new Firebase('https://jmartin-webhook.firebaseio.com/events')
-  , _ = require('lodash')
-  , actionableEventTypes = ['list_unsubscribe', 'link_unsubscribe', 'spam_complaint', 'bounce']
-  , moneyMakerEventTypes = ['open', 'click']
-  , hardBounceClasses = [10, 30, 90];
+  , FirebaseTokenGenerator = require('firebase-token-generator')
+  , uuid = require('node-uuid')
+  , _ = require('lodash');
 
-function processEventBatch(batch) {
-  batch.forEach(function(event) {
-    var eventGrouping = _.keys(event.msys)[0]
-      , eventType = event.msys[eventGrouping].type;
+/*
+ * Constructor method for creating our demo event processor
+ */
+var SparkyFire = function() {
+  var self = this;
 
-    // Processing for events which would lead to you cleaning up your recipient lists (ie unsubscribes)
-    if (_.includes(actionableEventTypes, eventType)) {
-      if (eventType === 'bounce') {
-        var bounceClass = event.msys[eventGrouping].bounce_class;
-        if (_.includes(hardBounceClasses, bounceClass)) {
-          console.log('🔥 🔥 🔥  Received ' + eventType + ' event from SparkPost!');
-        }
-      } else {
-        console.log('🔥 🔥 🔥  Received ' + eventType + ' event from SparkPost!');
-      }
-    }
+  if (!process.env.FIREBASE_URL || !process.env.FIREBASE_SECRET) {
+    console.error('Please set the URL and Secret of your Firebase instance as an environment variables!');
+    process.exit(-1);
+  }
 
-    // Processing for events which lead to revenue (ie opens and clicks)
-    if (_.includes(moneyMakerEventTypes, eventType)) {
-      console.log('💰 💰 💰  Received ' + eventType + ' event from SparkPost!');
+  this.db = new Firebase(process.env.FIREBASE_URL);
+  this.tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET);
+
+  // Create a JWT token for our process (https://www.firebase.com/docs/rest/guide/user-auth.html#section-token-generation)
+  this.token = this.tokenGenerator.createToken({uid: uuid.v4(), type: 'webhook-processing'});
+
+  this.db.authWithCustomToken(this.token, function(err) {
+    if (err) {
+      console.error('Unable to authenticate with your Firebase instance!', err);
+      process.exit(-1);
+    } else {
+      self.readPreviousBatches();
+      self.listenForBatches();
     }
   });
 };
 
+SparkyFire.prototype.readPreviousBatches = function() {
+  var self = this;
+  console.log('Reading batches that came in while we were down...');
 
-console.log('Authenticating with Firebase instance...');
-db.authWithCustomToken('Yx3r9poJxGazcQse9v82rPJyYweG90N1Yj5uSx9G', function(err, res) {
-  if (err) {
-    console.error('Unable to authenticate with Firebase:', err);
-  } else {
-    // Read from our backup on startup
-    db.once('value', function(rawEvents) {
-      console.log('Reading batches that came in while we were down...');
-
-      _.forOwn(rawEvents.val(), function(batch, key) {
-        processEventBatch(batch);
-        db.child(key).remove();
-      });
-
-      console.log('Finished replaying events, listening for new batches...');
+  // `value` returns the entire listing at one time
+  this.db.child('raw-events').once('value', function(batches) {
+    _.forOwn(batches.val(), function(batch, key) {
+      self.processBatch(batch);
+      self.db.child('raw-events').child(key).remove();
     });
 
-    // Listen for any new events to come in
-    db.on('child_added', function(rawEvents) {
-      processEventBatch(rawEvents.val());
-      db.child(rawEvents.key()).remove();
-    });
-  }
-});
+    console.log('Finished replay, waiting for new batches...');
+  });
+};
+
+SparkyFire.prototype.listenForBatches = function() {
+  var self = this;
+
+  this.db.child('raw-events').on('child_added', function(batch) {
+    self.processBatch(batch.val());
+    self.db.child('raw-events').child(batch.key()).remove();
+  });
+};
+
+SparkyFire.prototype.processBatch = function(batch) {
+  var self = this;
+
+  _.forEach(batch, function(event) {
+    var eventGrouping = _.keys(event.msys)[0]
+      , eventType = event.msys[eventGrouping].type
+      , processFunction = self.processEvent(eventType);
+
+    processFunction(event.msys[eventGrouping]);
+  });
+};
+
+SparkyFire.prototype.processEvent = function(eventType) {
+  var self = this;
+
+  var mapping = {
+    delay: self.processDelayEvent,
+    bounce: self.processBounceEvent,
+    delivery: self.processDeliveryEvent,
+    injection: self.processInjectionEvent,
+    sms_status: self.processSmsStatusEvent,
+    spam_complaint: self.processSpamComplaintEvent,
+    out_of_band: self.processOutOfBandEvent,
+    policy_rejection: self.processPolicyRejectionEvent,
+    click: self.processClickEvent,
+    open: self.processOpenEvent,
+    relay_injection: self.processRelayInjectionEvent,
+    relay_rejection: self.processRelayRejectionEvent,
+    relay_delivery: self.processRelayDeliveryEvent,
+    relay_tempfail: self.processRelayTempfailEvent,
+    relay_permfail: self.processRelayPermfailEvent,
+    generation_failure: self.processGenerationFailureEvent,
+    generation_rejection: self.processGenerationRejectionEvent,
+    list_unsubscribe: self.processListUnsubscribeEvent,
+    link_unsubscribe: self.processLinkUnsubscribeEvent
+  };
+
+  return mapping[eventType];
+};
+
+SparkyFire.prototype.processDelayEvent = function(event) {};
+
+SparkyFire.prototype.processBounceEvent = function(event) {};
+
+SparkyFire.prototype.processDeliveryEvent = function(event) {};
+
+SparkyFire.prototype.processInjectionEvent = function(event) {};
+
+SparkyFire.prototype.processSmsStatusEvent = function(event) {};
+
+SparkyFire.prototype.processSpamComplaintEvent = function(event) {};
+
+SparkyFire.prototype.processOutOfBandEvent = function(event) {};
+
+SparkyFire.prototype.processPolicyRejectionEvent = function(event) {};
+
+SparkyFire.prototype.processClickEvent = function(event) {};
+
+SparkyFire.prototype.processOpenEvent = function(event) {};
+
+SparkyFire.prototype.processRelayInjectionEvent = function(event) {};
+
+SparkyFire.prototype.processRelayRejectionEvent = function(event) {};
+
+SparkyFire.prototype.processRelayDeliveryEvent = function(event) {};
+
+SparkyFire.prototype.processRelayTempfailEvent = function(event) {};
+
+SparkyFire.prototype.processRelayPermfailEvent = function(event) {};
+
+SparkyFire.prototype.processGenerationFailureEvent = function(event) {};
+
+SparkyFire.prototype.processGenerationRejectionEvent = function(event) {};
+
+SparkyFire.prototype.processListUnsubscribeEvent = function(event) {};
+
+SparkyFire.prototype.processLinkUnsubscribeEvent = function(event) {};
+
+module.exports = new SparkyFire();
